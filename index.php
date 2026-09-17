8b25ee7708e3cb42b4c31cb01651e506dc737e4d
OsvaldoOlvera <125052448@upq.edu.mx>
2026-09-17 17:21:40 -0600

    Se agrego un archivo llamado indx.php con un codigo de etiquetas h

---
+++ index.php
@@ -0,0 +1,29 @@
+<!-- <?php
+    echo"Hola Mundo";
+?> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+
+        h1{
+            color: #1606f3;
+        }
+
+    </style>
+</head>
+<body>
+
+    <h1>Hola Mundo</h1>
+    <h2>Hola Mundo</h2>
+    <h3>Hola Mundo</h3>
+    <h4>Hola Mundo</h4>
+    <h5>Hola Mundo</h5>
+    <h6>Hola Mundo</h6>
+    
+</body>
+</html>
